63548aa9b3b16d2540b478608c1564003fa5b762
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-18 21:51:52 +0530

    All views or pages

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const port = 9000;
+const layouts = require('express-ejs-layouts');
 
 let app = express();
+
+// collecting form data
+app.use(express.urlencoded());
+
+//setting up views
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     console.log(`Server is up & running at port : ${port}`);
